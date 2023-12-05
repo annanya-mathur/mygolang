9f90195fa452efd1288881e8e0390ba32c212bbe
annanya <annanyasmathur2017@gmail.com>
2023-12-05 11:50:26 +0530

    Updated

--- 02variables/main.go
+++ 02variables/main.go
@@ -3,7 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var name string = "Abc"
+	var name string = "ABC"
 	fmt.Println(name)
+	fmt.Printf("Variable is of type: %T \n", name)
+
+	var num uint = 256
+	fmt.Println(num)
+	fmt.Printf("Variable is of type: %T \n", num)
+
+	//implicit type
+	var website = "https://github.com/annanya-mathur"
+	fmt.Println(website)
+
+	number := 3000.0
+	fmt.Println(number)
+	fmt.Printf("Variable is of type: %T \n", number)
 
 }
